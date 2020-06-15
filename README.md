41c72e4d15f894eaa3c1059323919c334a8366f0
Adrian-Daniel Bacanu <darkzatarra@yahoo.com>
2020-06-15 21:40:18 +0200

    Get all events by date

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AlienVault2MISP
 This should be a script which is able to get data from AlienVault and ingests it to a MISP server
+
+To create the timestamp file run this command:
+
+```date --date="1 days ago" "+%Y-%m-%dT%H:%M:%S.%N" > timestamp```
---
+++ otx_to_misp.py
@@ -0,0 +1,51 @@
+from OTXv2 import OTXv2
+from pandas.io.json import json_normalize
+from datetime import datetime, timedelta
+import requests
+from pymisp import PyMISP
+import time
+import json
+
+# service parameters
+otx_key = ''
+misp_url = ''
+misp_key = ''
+
+def readTimestamp():
+        fname = "timestamp"
+        f = open(fname, "r")
+        mtimestamp = f.read()
+        f.close()
+        return mtimestamp
+
+def saveTimestamp(timestamp=None):
+        mtimestamp = timestamp
+        if not timestamp:
+                mtimestamp = datetime.now().isoformat()
+
+        fname = "timestamp"
+        f = open(fname, "w")
+        f.write(mtimestamp)
+        f.close()
+
+
+
+if __name__ == "__main__":
+
+    otx = OTXv2(otx_key)
+
+    # The getall() method downloads all the OTX pulses and their assocciated indicators of compromise (IOCs) from your account.
+    # This includes all of the following:
+    # - OTX pulses to which you subscribed through the web UI
+    # - Pulses created by OTX users to whom you subscribe
+    # - OTX pulses you created.
+    # If this is the first time you are using your account, the download includes all pulses created by AlienVault.
+    # All users are subscribed to these by default.
+
+    mtime = readTimestamp()
+    pulses = otx.getsince(mtime)
+    print("Retrived %d pulses" % len(pulses))
+
+    # Summary of information retrieved from OTX
+    for p in pulses:
+        print(json.dumps(p, indent=2, sort_keys=True))
