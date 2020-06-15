26fcf1bac377fb9ffed1f4695e6666b9a035d602
alexandru-grajdeanu <grajdeanu.alex@gmail.com>
2020-06-15 23:26:07 +0300

    Refactored otx_to_misp

--- otx_to_misp.py
+++ otx_to_misp.py
@@ -1,3 +1,15 @@
+"""
+# The getall() method downloads all the OTX pulses and their associated
+    # indicators of compromise (IOCs) from your account.
+    # This includes all of the following:
+    # - OTX pulses to which you subscribed through the web UI
+    # - Pulses created by OTX users to whom you subscribe
+    # - OTX pulses you created.
+    # If this is the first time you are using your account, the download
+      includes all pulses created by AlienVault.
+    # All users are subscribed to these by default.
+"""
+
 from datetime import datetime, timedelta
 
 from OTXv2 import OTXv2
@@ -11,44 +23,35 @@ import time
 import json
 
 
-# service parameters
 misp_url = ''
 
 
-def readTimestamp():
-        fname = "timestamp"
-        f = open(fname, "r")
-        mtimestamp = f.read()
-        f.close()
-        return mtimestamp
-
-def saveTimestamp(timestamp=None):
-        mtimestamp = timestamp
-        if not timestamp:
-                mtimestamp = datetime.now().isoformat()
-
-        fname = "timestamp"
-        f = open(fname, "w")
-        f.write(mtimestamp)
-        f.close()
+def read_timestamp():
+    with open("timestamp") as input_file:
+        m_timestamp = input_file.read()
+        return m_timestamp
 
 
-if __name__ == "__main__":
+def save_timestamp(timestamp=None):
+    m_timestamp = timestamp
+    if not timestamp:
+        m_timestamp = datetime.now().isoformat()
 
+    with open("timestamp", "w") as output_file:
+        output_file.write(m_timestamp)
+
+
+def main():
     otx = OTXv2(OTX_KEY)
 
-    # The getall() method downloads all the OTX pulses and their assocciated indicators of compromise (IOCs) from your account.
-    # This includes all of the following:
-    # - OTX pulses to which you subscribed through the web UI
-    # - Pulses created by OTX users to whom you subscribe
-    # - OTX pulses you created.
-    # If this is the first time you are using your account, the download includes all pulses created by AlienVault.
-    # All users are subscribed to these by default.
-
-    mtime = readTimestamp()
+    mtime = read_timestamp()
     pulses = otx.getsince(mtime)
-    print("Retrived %d pulses" % len(pulses))
+    print("Retrieved {} pulses".format(len(pulses)))
 
     # Summary of information retrieved from OTX
     for p in pulses:
         print(json.dumps(p, indent=2, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
